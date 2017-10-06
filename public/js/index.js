3022eec82be6ff61529f262c46cf69f6220ec891
xxxgitone <xxxgit@sina.com>
2017-10-06 22:15:56 +0800

    完成底部导航

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,15 @@
 $(function() {
-  $('a').on('click', function() {
-    console.log(this)
+  var $tab_panels = $('.weui-tab__panel div')
+  var $tabbar_item = $('.weui-tabbar a')
+  var lastIndex = 0
+  $('a').on('tap', function() {
+    var index = $(this).index()
+    if (index !== lastIndex) {
+      $($tabbar_item[lastIndex]).removeClass('weui-bar__item_on')
+      $($tab_panels[lastIndex]).css('display', 'none')
+      $(this).addClass('weui-bar__item_on')
+      $($tab_panels[index]).css('display', 'block')
+      lastIndex = index
+    }
   })
 })
