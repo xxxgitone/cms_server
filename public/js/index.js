f808a263d0ce3a556c5c9e14f9ba18ef7fe3f934
xxxgitone <xxxgit@sina.com>
2017-10-10 15:51:56 +0800

    首页课程列表渲染

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 $(function() {
-  var $tab_panels = $('.weui-tab__panel div')
+  var $tab_panels = $('.weui-tab__panel .tab-item')
   var $tabbar_item = $('.weui-tabbar a')
   var lastIndex = 0
   $('a').on('tap', function() {
@@ -14,6 +14,21 @@ $(function() {
   })
 
   $.get('/api/courses', function (data) {
-    console.log(data)
+    var course = data.course
+    console.log(course)
+    var html = course.map(function (item) {
+      return `
+        <a href="course/${item._id}" class="weui-media-box weui-media-box_appmsg">
+          <div class="weui-media-box__hd">
+            <img class="weui-media-box__thumb" src="http://dummyimage.com/200x200/f2d479/b179f2&text=C" alt="">
+          </div>
+          <div class="weui-media-box__bd">
+            <h4 class="weui-media-box__title">${item.courseName}</h4>
+            <p class="weui-media-box__desc">${item.introduction}</p>
+          </div>
+        </a>
+      `
+    }).join('')
+    $('.weui-panel__bd').html(html)
   })
 })
