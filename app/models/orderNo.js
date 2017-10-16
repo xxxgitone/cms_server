bf88db303a79d2be53f10bd3b770cba023f3a59d
xxxgitone <xxxgit@sina.com>
2017-10-16 20:53:20 +0800

    订单编号model

--- app/models/orderNo.js
+++ app/models/orderNo.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const OrderNoSchema = new Schema({
-  name: 'orderNo',
-  id: 10000
+  name: String,
+  id: Number
 })
 
 const OrderNo = mongoose.model('OrderNo', OrderNoSchema)
